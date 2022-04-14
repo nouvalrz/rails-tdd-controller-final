8d793e691c61229c82e6cef808b569e13ec8e0d8
Nouval Rizky <nouvalr@gmail.com>
2022-04-14 16:23:30 +0000

    Refactor the Spec to Use Factories

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,9 @@
 class Food < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
+  
 end
