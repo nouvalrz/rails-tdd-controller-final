227d7883ea6769d4770f4fb9e279d005244a658a
Nouval Rizky <nouvalr@gmail.com>
2022-04-14 17:06:03 +0000

    Add validation for foods controller for index method

---
+++ spec/controllers/foods_controllers_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe FoodsController do
+  describe 'GET #index' do
+    context 'with params[:letter]' do
+      it "populates an array of foods starting with the letter" do
+        nasi_uduk = create(:food, name: "Nasi Uduk")
+        kerak_telor = create(:food, name: "Kerak Telor")
+
+        get :index, params: { letter: "N" }
+        expect(assigns(:foods)).to match_array([nasi_uduk])
+      end
+      it "renders the :index template" do
+        get :index, params: {letter: "N"}
+        expect(assigns(:foods)).to render_template :index
+      end
+    end
+
+    context 'without params[:letter]' do
+      it "populates an array of all foods" do
+        nasi_uduk = create(:food, name: "Nasi Uduk")
+        kerak_telor = create(:food, name: "Kelar Telor")
+        get :index
+        expect(assigns(:foods)).to match_array([nasi_uduk, kerak_telor])
+      end
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+  end
+end
