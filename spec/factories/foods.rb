5de2851ca3d9f0913e208b07175be4f1f304a09f
Nouval Rizky <nouvalr@gmail.com>
2022-04-14 16:26:51 +0000

    Use faker for name attribute in factory bot

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :food do
-    name { "Nasi Uduk" }
+    name { Faker::Food.dish }
     description { "Betawi style steamed rice cooked in coconut milk. Delicious!" }
     price { 10000.0 }
   end
